7f4e340ea5b0b6cc9bf9008585ba2f57238c70de
Alena Varkockova <varkockova.a@gmail.com>
2020-04-19 10:07:42 +0200

    Infix expressions

--- pkg/eval/eval.go
+++ pkg/eval/eval.go
@@ -29,6 +29,11 @@ func Eval(node ast.Node) object.Object {
 		default:
 			newError("unknown operator %s. %v", prefix.Operator, prefix)
 		}
+	case *ast.InfixExpression:
+		infix, _ := node.(*ast.InfixExpression)
+		left := Eval(infix.Left)
+		right := Eval(infix.Right)
+		return evalInfixOperator(left, right, infix.Operator)
 	case *ast.ExpressionStatement:
 		exp, _ := node.(*ast.ExpressionStatement)
 		return Eval(exp.Expression)
@@ -37,10 +42,88 @@ func Eval(node ast.Node) object.Object {
 		program, _ := node.(*ast.Program)
 		for _, stmt := range program.Statements {
 			result = Eval(stmt)
+			switch result.(type) {
+			case *object.Error:
+				return result
+			}
 		}
 		return result
 	}
 	return nil
+}
+
+func evalInfixOperator(left object.Object, right object.Object, operator string) object.Object {
+	switch operator {
+	case "+":
+		leftInt, leftok := left.(*object.Integer)
+		rightInt, rightok := right.(*object.Integer)
+		if !leftok || !rightok {
+			return newError("infix operator + works only with integers. Got %s+%s", left.Type(), right.Type())
+		}
+		return &object.Integer{leftInt.Value + rightInt.Value}
+	case "-":
+		leftInt, leftok := left.(*object.Integer)
+		rightInt, rightok := right.(*object.Integer)
+		if !leftok || !rightok {
+			return newError("infix operator - works only with integers. Got %s-%s", left.Type(), right.Type())
+		}
+		return &object.Integer{leftInt.Value - rightInt.Value}
+	case "*":
+		leftInt, leftok := left.(*object.Integer)
+		rightInt, rightok := right.(*object.Integer)
+		if !leftok || !rightok {
+			return newError("infix operator * works only with integers. Got %s*%s", left.Type(), right.Type())
+		}
+		return &object.Integer{leftInt.Value * rightInt.Value}
+	case "/":
+		leftInt, leftok := left.(*object.Integer)
+		rightInt, rightok := right.(*object.Integer)
+		if !leftok || !rightok {
+			return newError("infix operator * works only with integers. Got %s*%s", left.Type(), right.Type())
+		}
+		return &object.Integer{leftInt.Value / rightInt.Value}
+	default:
+		return evalEqualityExpression(left, right, operator)
+	}
+}
+
+func evalEqualityExpression(left object.Object, right object.Object, operator string) object.Object {
+	if left.Type() != right.Type() {
+		return object.FALSE
+	}
+	if left.Type() == object.INTEGER {
+		leftVal := left.(*object.Integer).Value
+		rightVal := right.(*object.Integer).Value
+		switch operator {
+		case "==":
+			return boolResultToObject(leftVal == rightVal)
+		case "!=":
+			return boolResultToObject(leftVal != rightVal)
+		case ">":
+			return boolResultToObject(leftVal > rightVal)
+		case "<":
+			return boolResultToObject(leftVal < rightVal)
+		default:
+			return newError("unsupported operator %s%s%s", left.Type(), operator, right.Type())
+		}
+	} else {
+		switch operator {
+		case "==":
+			return boolResultToObject(left == right)
+		case "!=":
+			return boolResultToObject(left != right)
+		default:
+			return newError("unsupported operator %s%s%s", left.Type(), operator, right.Type())
+		}
+	}
+	return nil
+}
+
+func boolResultToObject(b bool) object.Object {
+	if b {
+		return object.TRUE
+	}
+	return object.FALSE
 }
 
 func evalBang(value object.Object) object.Object {
--- pkg/eval/eval_test.go
+++ pkg/eval/eval_test.go
@@ -16,6 +16,17 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"10", 10},
 		{"-5", -5},
 		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -52,17 +63,35 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"true", true},
 		{"false", false},
 		{"!false", true},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
-		testBooleanObject(t, evaluated, tt.expected)
+		testBooleanObject(t, evaluated, tt.expected, tt.input)
 	}
 }
 
-func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+func testBooleanObject(t *testing.T, obj object.Object, expected bool, input string) bool {
 	result, ok := obj.(*object.Boolean)
 	if !ok {
-		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		t.Errorf("%s: object is not Boolean. got=%T (%+v)", input, obj, obj)
 		return false
 	}
 	if result.Value != expected {
@@ -71,4 +100,61 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 		return false
 	}
 	return true
+}
+
+func TestErrorHandling(t *testing.T) {
+	tests := []struct {
+		input string
+		expectedMessage string
+	}{
+		{
+			"5 + true;",
+			"infix operator + works only with integers. Got INTEGER+BOOLEAN",
+		},
+		{
+			"5 + true; 5;",
+			"infix operator + works only with integers. Got INTEGER+BOOLEAN",
+		},
+		{
+			"-true",
+			"unknown operator: -BOOLEAN",
+		},
+		{
+			"true + false;",
+			"infix operator + works only with integers. Got BOOLEAN+BOOLEAN",
+		},
+		{
+			"5; true + false; 5",
+			"infix operator + works only with integers. Got BOOLEAN+BOOLEAN",
+		},
+		/*{
+			"if (10 > 1) { true + false; }",
+			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			`
+132
+if (10 > 1) {
+if (10 > 1) {
+return true + false;
+}
+return 1;
+}
+`,
+			"unknown operator: BOOLEAN + BOOLEAN",
+		},*/
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		errObj, ok := evaluated.(*object.Error)
+		if !ok {
+			t.Errorf("no error object returned. got=%T(%+v)",
+				evaluated, evaluated)
+			continue
+		}
+		if errObj.Message != tt.expectedMessage {
+			t.Errorf("wrong error message. expected=%q, got=%q",
+				tt.expectedMessage, errObj.Message)
+		}
+	}
 }
